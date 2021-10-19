995994c5ef5537ddbf7f350f6b810c4922c24821
Can Udomcharoenchaikit <can@Cans-MacBook-Pro-2.local>
2021-10-20 00:22:14 +0700

    Add distiallation training set creator

---
+++ utils/create_distill_train_set.py
@@ -0,0 +1,40 @@
+import jsonlines
+import sys, getopt
+
+
+def main(argv):
+    output_path = ''
+
+    
+    try:
+       opts, args = getopt.getopt(argv,"ht:p:o:",["help","train_path=","pred_path=","output_path="])
+    except getopt.GetoptError:
+       print('create_distill_train_set.py -t <train_path> -p <pred_path> -o <output_path>')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print('create_distill_train_set.py -t <train_path> -p <pred_path> -o <output_path>')
+          sys.exit()
+       elif opt in ("-t", "--train_path"):
+          train_path = arg    
+       elif opt in ("-p", "--pred_path"):
+          pred_path = arg               
+       elif opt in ("-o", "--output_path"):
+          output_path = arg
+
+    # load files here
+    out = []
+    with jsonlines.open(train_path) as f1,jsonlines.open(pred_path) as f2:
+        for line, line_pred in zip(f1.iter(),f2.iter()):
+            line['logits'] = line_pred['logits']
+            line['probs'] = line_pred['probs']
+            out.append(line)
+
+
+
+    with jsonlines.open(output_path, 'w') as writer:
+        writer.write_all(out)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+      
