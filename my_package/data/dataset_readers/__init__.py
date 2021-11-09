1b3d99b64825b2b7bdaa53949f8c82512b8fab2e
Patomp <patomporn.pa@backyard.in.trh>
2021-11-09 11:22:25 +0700

    Add weighted datareader for FEVER and QQP

--- my_package/data/dataset_readers/__init__.py
+++ my_package/data/dataset_readers/__init__.py
@@ -1,9 +1,12 @@
-from my_package.data.dataset_readers  import counterfactual_reader, counterfactual_reader_mask_ol, counterfactual_reader_hypo
-from my_package.data.dataset_readers  import grad_reverse_overlap_readers
-from my_package.data.dataset_readers  import overlap_score_reader
-from my_package.data.dataset_readers  import sample_weight_reader
+from my_package.data.dataset_readers import counterfactual_reader, counterfactual_reader_mask_ol, counterfactual_reader_hypo
+from my_package.data.dataset_readers import grad_reverse_overlap_readers
+from my_package.data.dataset_readers import overlap_score_reader
+from my_package.data.dataset_readers import sample_weight_reader
 
-from my_package.data.dataset_readers  import distill_reader
+from my_package.data.dataset_readers import distill_reader
 
 from my_package.data.dataset_readers.fever import reader as fever_reader
+from my_package.data.dataset_readers.fever import weighted_reader as weighted_fever_reader
+
 from my_package.data.dataset_readers.qqp import reader as qqp_reader
+from my_package.data.dataset_readers.qqp import weighted_reader as weighted_qqp_reader
