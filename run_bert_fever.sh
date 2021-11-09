c8e185fb4ceb0f35b33352dadc3032c935ba1355
KornWtp <elecwpr@gmail.com>
2021-11-09 11:32:30 +0000

    Add .jsonnet fine-tune BERT on fact_verification

---
+++ run_bert_fever.sh
@@ -0,0 +1,21 @@
+
+#This example, show fine-tune BERT base on mnli use allennlp 
+
+export CUDA_VISIBLE_DEVICES=7
+allennlp train configs/fact_verification/fever_bert_base_1.jsonnet \
+-s output_models/bert_base_fever \
+--include-package my_package
+
+# This example, show evaluation BERT base on fever_symmetric_generated
+allennlp evaluate output_models/bert_base_fever/model.tar.gz \
+ data/fact_verification/fever_symmetric_generated.jsonl \
+ --output-file outputs_eval_fever/fever_symmetric_generated.json  \
+ --cuda-device 0 \
+ --include-package my_package
+
+# This example, show evaluation BERT base on fever
+allennlp evaluate output_models/bert_base_fever/model.tar.gz \
+ data/fact_verification/fever.dev.jsonl \
+ --output-file outputs_eval_fever/fever_dev.json  \
+ --cuda-device 0 \
+ --include-package my_package
