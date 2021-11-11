a66025d6e864b27a85a2c7c2ac32146b5ccc069a
KornWtp <elecwpr@gmail.com>
2021-11-11 04:45:43 +0000

    Add fever test v2

--- data/fact_verification/download.sh
+++ data/fact_verification/download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## Ref: https://github.com/TalSchuster/FeverSymmetric
+wget -O fever.train.jsonl https://www.dropbox.com/s/v1a0depfg7jp90f/fever.train.jsonl
+wget -O fever.dev.jsonl https://www.dropbox.com/s/bdwf46sa2gcuf6j/fever.dev.jsonl
 
-wget https://raw.githubusercontent.com/TalSchuster/FeverSymmetric/master/symmetric_v0.1/fever_symmetric_generated.jsonl
-wget https://www.dropbox.com/s/v1a0depfg7jp90f/fever.train.jsonl
-wget https://www.dropbox.com/s/bdwf46sa2gcuf6j/fever.dev.jsonl
+## Ref: https://github.com/TalSchuster/FeverSymmetric
+wget -O fever_symmetric_v0.1.jsonl https://raw.githubusercontent.com/TalSchuster/FeverSymmetric/master/symmetric_v0.1/fever_symmetric_generated.jsonl
+wget -O fever_symmetric_v0.2.jsonl https://raw.githubusercontent.com/TalSchuster/FeverSymmetric/master/symmetric_v0.2/fever_symmetric_test.jsonl
