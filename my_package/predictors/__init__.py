6ddb6207fbcb59f509667e60ea4555841b5c5be0
c4n <udomc.can@gmail.com>
2022-01-05 16:19:03 +0700

    update predictor textual entialment

--- my_package/predictors/__init__.py
+++ my_package/predictors/__init__.py
@@ -1,1 +1,1 @@
-from my_package.predictors  import cf_predictor, cf_predictor_entropy, cf_predictor_entropy_temp_scaled, dev_temp_scaled, cf_predictor_entropy_distill
+from my_package.predictors  import cf_predictor, cf_predictor_entropy, cf_predictor_entropy_temp_scaled, dev_temp_scaled, cf_predictor_entropy_distill, textual_entialment2
