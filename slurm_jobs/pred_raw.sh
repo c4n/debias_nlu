9d7e8face36662d1cbbc3087e7d782aac16bdbdf
Patomp <patomporn.pa@backyard.in.trh>
2022-01-18 15:07:42 +0700

    Update all FEVER and QQP bash scripts

---
+++ slurm_jobs/pred_raw.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+# DATASET=${1:-paraphrase_identification}
+DATASET=${1:-fact_verification}
+
+# MODEL_PATHS=(
+#     "results/outputs_qqp_bert_base_1"
+#     "results/outputs_qqp_weighted_bert_1"
+#     "results/outputs_qqp_poe_bert_1"
+# )
+# MODEL_PATHS=(
+#     "results/outputs_fever_bert_base_1"
+#     "results/outputs_fever_weighted_bert_1"
+#     "results/outputs_fever_poe_bert_1"
+# )
+MODEL_PATHS=(
+    "results/outputs_fever_weighted_bert_1"
+    "results/outputs_fever_poe_bert_1"
+)
+
+SEEDs=( 2 3 4 5 6 )
+
+for model_path in "${MODEL_PATHS[@]}"
+do
+    for seed in "${SEEDs[@]}"
+    do
+        bash slurm_jobs/${DATASET}/job_get_raw.sub ${model_path}_seed${seed}3370
+    done
+done
+
