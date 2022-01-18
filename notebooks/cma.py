b627c9fa0a3b22f99ac9b663ac9bb0946cfaa4e0
Patomp <patomporn.pa@backyard.in.trh>
2022-01-18 15:09:03 +0700

    Update CMA python scripts

--- notebooks/cma.py
+++ notebooks/cma.py
@@ -1,14 +1,14 @@
 import os
-from typing import Callable, List, Union
+from typing import Callable, List, Union, Tuple
 
 import pandas as pd
 import numpy as np
 from scipy.special import softmax
 from scipy.special import expit
+from sklearn.metrics import f1_score
 import fuse
-import causal_utils
 import glob
-from kl_general import sharpness_correction
+from kl_general import sharpness_correction, DEFAULT_CONFIG as ESTIMATE_C_DEFAULT_CONFIG
 import pickle
 
 
@@ -19,14 +19,14 @@ BIAS_MODEL_DICT = {
     'mnli_dev_mm': 'nli/test_prob_korn_lr_overlapping_sample_weight_3class.jsonl',
     'mnli_hans': 'nli/hans_prob_korn_lr_overlapping_sample_weight_3class.jsonl',
 
-    'fever_train': 'fact_verification/fever.train.jsonl',
-    'fever_dev': 'fact_verification/fever.dev.jsonl',
-    'fever_sym1': 'fact_verification/fever_symmetric_v0.1.test.jsonl',
-    'fever_sym2': 'fact_verification/fever_symmetric_v0.2.test.jsonl',
+    'fever_train': 'weighted_fever.train.jsonl',
+    'fever_dev': 'weighted_fever.dev.jsonl',
+    'fever_sym1': 'weighted_fever_symmetric_v0.1.test.jsonl',
+    'fever_sym2': 'weighted_fever_symmetric_v0.2.test.jsonl',
 
-    'qqp_train': 'paraphrase_identification/qqp.train.jsonl',
-    'qqp_dev': 'paraphrase_identification/qqp.dev.jsonl',
-    'qqp_paws': 'paraphrase_identification/paws.dev_and_test.jsonl',
+    'qqp_train': 'weighted_qqp.train.jsonl',
+    'qqp_dev': 'weighted_qqp.dev.jsonl',
+    'qqp_paws': 'weighted_paws.dev_and_test.jsonl',
 }
 
 BERT_MODEL_RESULT_DICT = {
@@ -45,32 +45,56 @@ BERT_MODEL_RESULT_DICT = {
 }
 
 
-def format_label(label):
-    if label == "entailment":
-        return "entailment"
-    else:
-        return "non-entailment"
-
-
-def get_ans(ans: int, test_set: str) -> str:
+def get_ans(ans: int, test_set: str) -> Union[int, str]:
     if test_set == 'mnli_hans':
         if ans == 0:
             return 'entailment'
         else:
             return 'non-entailment'
+
     if test_set == 'mnli_test':
         gt_key = {0: "entailment", 1: "contradiction", 2: "neutral"}
         return gt_key[ans]
-    elif test_set == 'fever':
-        if ans == 2:  # Ref from dictionary in result dir
-            return 'contradiction'  # since we mapped it in the Allen's reader
-        return 'non-contradiction'
-    elif test_set == 'qqp':
-        if ans == 1:  # Ref from dictionary in result dirs
-            return 'paraphrase'
-        return 'non-paraphrase'
-    else:
-        raise NotImplementedError("Does not support test_set: %s" % test_set)
+
+    if 'fever' in test_set:
+        gt_key = {0: "SUPPORTS", 1: "NOT ENOUGH INFO", 2: "REFUTES"}
+        return gt_key[ans]
+
+    if 'qqp' in test_set:
+        return ans
+
+    raise NotImplementedError("Does not support test_set: %s" % test_set)
+
+
+def get_bias_effect(
+    nde: List[List[float]],
+    nie: List[List[float]],
+    tie: List[float],
+    te: List[List[float]],
+    test_set: str
+) -> Tuple[Union[List[float], List[List[float]]]]:
+    if "nli" in test_set:
+        return (
+            nde[0][0],
+            nie[0][0],
+            tie[0],
+            te[0][0]
+        )
+    elif 'fever' in test_set:
+        return (
+            nde[0][2],
+            nie[0][2],
+            tie[2],
+            te[0][2]
+        )
+    elif 'qqp' in test_set:
+        return (
+            nde[0][1],
+            nie[0][1],
+            tie[1],
+            te[0][1]
+        )
+    raise NotImplementedError("Does not support test_set: %s" % test_set)
 
 
 def get_c(
@@ -82,6 +106,7 @@ def get_c(
     model_val_pred_file: str = 'raw_m.jsonl',
     bias_probs_key: str = 'bias_probs',
     model_probs_key: str = 'probs',
+    config: dict = ESTIMATE_C_DEFAULT_CONFIG
 ) -> List[float]:
     df_bias_dev = pd.read_json(
         os.path.join(data_path, bias_val_pred_file),
@@ -98,7 +123,13 @@ def get_c(
     for p, h in zip(df_bert_dev[model_probs_key], ya1x0_dev):
         new_ya1x1 = fusion(np.array(p), h)
         ya1x1prob_dev.append(new_ya1x1)
-    c = sharpness_correction(bias_dev_score, ya1x1prob_dev)
+    c = sharpness_correction(
+        bias_dev_score,
+        ya1x1prob_dev,
+        config=config
+    )
+    print("c: ", c)
+    print("softmax(c): ", softmax(c))
     return c
 
 
@@ -112,11 +143,12 @@ def _default_model_pred(
 
 def report_CMA(
     model_path: str,
-    task: str,
+    task: str,  # MNLI, FEVER, QQP
     data_path: str,
     test_set: str,
     fusion: Callable[[PROB_T], PROB_T] = fuse.sum_fuse,
     input_a0: List[float] = [0, 0, 0.41997876976119086],
+    estimate_c_config: dict = ESTIMATE_C_DEFAULT_CONFIG,
 
     correction: bool = False,
     bias_probs_key: str = 'bias_probs',
@@ -126,15 +158,19 @@ def report_CMA(
     bias_val_pred_file: str = 'dev_prob_korn_lr_overlapping_sample_weight_3class.jsonl',
     model_val_pred_file: str = 'raw_m.jsonl',
 
-    TIE_ratio_threshold: float = 9999
+    TIE_ratio_threshold: float = 9999,
+
+    seed_path: List[str] = None
 ) -> None:
     '''
         Arguments:
+            - test_set: test, challenge set
             - bias_val_pred_file: val set from bias model (including probs)
             - model_val_pred_file: val set from BERT model (including probs)
     '''
     # load predictions from bias model (e.g., logistic regression)
     assert test_set in BIAS_MODEL_DICT.keys()
+    print(os.path.join(data_path, BIAS_MODEL_DICT[test_set]))
     df_bias_model = pd.read_json(
         os.path.join(data_path, BIAS_MODEL_DICT[test_set]),
         lines=True
@@ -143,22 +179,29 @@ def report_CMA(
     a1 = np.array(a1)
 
     # get a list of all seed dir
-    to_glob = model_path + task + '/*/'
-    seed_path = glob.glob(to_glob)  # list of model dir for all seeds
+    if not seed_path:
+        to_glob = model_path + task + '/*/'
+        seed_path = glob.glob(to_glob)  # list of model dir for all seeds
 
     # init list to store results
     TE_explain = []
     TIE_explain = []
     factual_scores = []
     TIE_scores = []
+    TIE_f1 = {}
     NIE_explain = []
     NIE_scores = []
+    NIE_f1 = {}
     INTmed_explain = []
     INTmed_scores = []
+    INTmed_f1 = {}
     my_causal_query = []
+    my_causal_f1 = {}
 
     # get avg score
     for seed_idx in range(len(seed_path)):
+        print(os.path.join(seed_path[seed_idx],
+                           BERT_MODEL_RESULT_DICT[test_set]))
         df_train = pd.read_json(
             os.path.join(seed_path[seed_idx],
                          BERT_MODEL_RESULT_DICT[test_set]),
@@ -178,7 +221,8 @@ def report_CMA(
                 bias_val_pred_file=bias_val_pred_file,
                 model_val_pred_file=model_val_pred_file,
                 bias_probs_key=bias_probs_key,
-                model_probs_key='probs'
+                model_probs_key='probs',
+                config=estimate_c_config
             )
         # fusion to create ya1x0
         ya1x0prob = fusion(a1, x0)
@@ -205,6 +249,8 @@ def report_CMA(
 
         # {0:"entailment",1:"contradiction",2:"neutral"}
         labels = df_bias_model[ground_truth_key]
+        unique_labels = labels.unique().tolist()
+        print("unique_labels: ", unique_labels)
 
         # to offset samples with no ground truth from accuracy calculation
         offset = 0
@@ -213,6 +259,8 @@ def report_CMA(
             offset = df_bias_model[ground_truth_key].value_counts()['-']
         # CMA
         a0 = model_pred_method(_input=input_a0)  # input_a0?
+        # print("input_a0: ", input_a0)
+        # print("a0: ", a0)
         ya0x0 = fusion(a0, x0)
         # to measure accuracy
         factual_pred_correct = []
@@ -226,6 +274,10 @@ def report_CMA(
         all_NIE = []
         all_NDE = []
         all_INTmed = []
+        tie_y_preds = []
+        nie_y_preds = []
+        intmed_y_preds = []
+        my_causal_y_preds = []
         for i in range(len(labels)):
             ya1x1 = ya1x1prob[i]
             ya1x0 = ya1x0prob[i]
@@ -235,39 +287,55 @@ def report_CMA(
             TIE = ya1x1 - ya1x0
             NIE = ya0x1 - ya0x0
             INTmed = TIE - NIE
-            # factual
+            # factual ## Todo: wrong index for FEVER
             factual_ans = np.argmax(x1[i])
             factual_ans = get_ans(factual_ans, test_set)
+            assert type(factual_ans) == type(labels[i])
+            # print(x1[i], factual_ans, labels[i])
+            # print("df_bert.iloc[i]: ", df_bert.iloc[i])
+            # print("df_bias_model.iloc[i]: ", df_bias_model.iloc[i])
             factual_correct = factual_ans == labels[i]
             factual_pred_correct.append(factual_correct)
             # TIE
             TIE_ans = np.argmax(TIE)
             TIE_ans = get_ans(TIE_ans, test_set)
+            assert type(TIE_ans) == type(labels[i])
+            tie_y_preds.append(TIE_ans)
             TIE_correct = TIE_ans == labels[i]
             TIE_pred_correct.append(TIE_correct)
             # INTmed
             INTmed_ans = np.argmax(INTmed[0])
             INTmed_ans = get_ans(INTmed_ans, test_set)
+            assert type(INTmed_ans) == type(labels[i])
+            intmed_y_preds.append(INTmed_ans)
             INTmed_correct = INTmed_ans == labels[i]
             INTmed_pred_correct.append(INTmed_correct)
             # NIE
             NIE_ans = np.argmax(NIE[0])
             NIE_ans = get_ans(NIE_ans, test_set)
+            assert type(NIE_ans) == type(labels[i])
+            nie_y_preds.append(NIE_ans)
             NIE_correct = NIE_ans == labels[i]
             NIE_pred_correct.append(NIE_correct)
 
-            # save
-            all_NDE.append(NDE[0][0])
-            all_NIE.append(NIE[0][0])
-            all_TIE.append(TIE[0])
-            all_TE.append(TE[0][0])
+            # save ## Todo: map bias class to correct index for each dataset
+            bias_nde, bias_nie, bias_tie, bias_te = get_bias_effect(
+                nde=NDE, nie=NIE, tie=TIE, te=TE, test_set=test_set
+            )
+            all_NDE.append(bias_nde)
+            all_NIE.append(bias_nie)
+            all_TIE.append(bias_tie)
+            all_TE.append(bias_te)
             all_INTmed.append((INTmed[0][0]))
             if (TIE[0]/TE[0][0]) < TIE_ratio_threshold:
                 cf_ans = np.argmax(np.array(x1[i]-a1[i]))
                 cf_ans = get_ans(cf_ans, test_set)
+                assert type(cf_ans) == type(labels[i])
                 cf_correct = cf_ans == labels[i]
             else:
+                assert type(factual_ans) == type(labels[i])
                 cf_correct = factual_ans == labels[i]
+            my_causal_y_preds.append(cf_ans)
             pred_correct.append(cf_correct)
 
         total_sample = len(labels) - offset
@@ -283,6 +351,20 @@ def report_CMA(
         print(np.array(all_TIE).mean(), (np.array(all_TIE).std()))
         print(np.array(all_INTmed).mean(), (np.array(all_INTmed).std()))
         print(np.array(all_NIE).mean(), (np.array(all_NIE).std()))
+        # F1 score
+        for x_f1, x_y_preds in zip(
+            [TIE_f1, NIE_f1, INTmed_f1, my_causal_f1],
+            [tie_y_preds, nie_y_preds, intmed_y_preds, my_causal_y_preds]
+        ):
+            f1_scores = f1_score(
+                y_true=labels, y_pred=x_y_preds,
+                average=None, labels=unique_labels
+            )
+            for label, f1 in zip(unique_labels, f1_scores):
+                try:
+                    x_f1[label].append(f1)
+                except KeyError:
+                    x_f1[label] = [f1, ]
 
     print('factual score:')
     print(factual_scores, np.array(factual_scores).mean(),
@@ -293,6 +375,8 @@ def report_CMA(
     print(TIE_explain, np.array(TIE_explain).mean(), np.array(TIE_explain).std())
     print("TIE acc:")
     print(TIE_scores, np.array(TIE_scores).mean(), np.array(TIE_scores).std())
+    print("TIE F1:", TIE_f1, {"%s_mean_sd" % k: [np.mean(v), np.std(v)]
+          for k, v in TIE_f1.items()})
     print("NIE:")
     print(NIE_explain, np.array(NIE_explain).mean(), np.array(NIE_explain).std())
     print("NIE acc:")
@@ -306,3 +390,5 @@ def report_CMA(
     print("my query:")
     print(my_causal_query, np.array(my_causal_query).mean(),
           np.array(my_causal_query).std())
+    print("my query F1:", my_causal_f1, {
+          "%s_mean_sd" % k: [np.mean(v), np.std(v)] for k, v in my_causal_f1.items()})
